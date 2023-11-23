f7b07742bda4554432cd219d6c5c8aa6c328b3a2
viktorvoltz <vctrchinyeaka@gmail.com>
2023-11-23 13:09:54 +0100

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/viktorvoltz/go-tut
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func concat(s1, s2 string) string {
+	return s1 + s2
+}
+
+func main() {
+	fmt.Println(concat("hello word", " nonso"))
+}
